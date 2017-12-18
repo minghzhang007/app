1f8852d0daf681c70de7dd0a40af17d5ea1bfb7c
minghzhang@163.com <minghzhang@163.com>
2017-12-18 20:24:27 +0800

    Initial commit

---
+++ backend/src/main/java/com/lewis/springbootvue/Application.java
@@ -0,0 +1,15 @@
+package com.lewis.springbootvue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangminghua
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
